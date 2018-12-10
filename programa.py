e02aa41275b82ce04b1b4b9061ee6b2f6f61ba9a
dougfonseca <45559794+dougfonseca@users.noreply.github.com>
2018-12-10 13:44:31 -0300

    GUI adicionado junto de rotinas de funcionamento

--- programa.py
+++ programa.py
@@ -1,70 +1,91 @@
-import os  # biblioteca para manipular os arquivos
+import os, sys # biblioteca para manipular os arquivos
 import wave # biblioteca para trabalhar com arquivos .wav
 import numpy as np # biblioteca para manipular os vetores/canais de áudio (pip install numpy)
-import soundfile as sf  # biblioteca para manipular os áudios (pip install pysoundfile)
+import soundfile as sf # biblioteca para manipular os áudios (pip install pysoundfile)
 import matplotlib.pyplot as plt # biblioteca para plotar a função (pip install -U matplotlib)
+from tkinter import * # biblioteca para a interface gráfica
+from tkinter import filedialog
 
-pasta = str(input("Digite o caminho completo da pasta que deseja manipular os arquivos de áudio: "))
-### Comando que vai para a pasta destino onde estão os arquivos ###
-os.chdir('%s'%pasta)
-### Comando que lista os arquivos que estão no diretório ###
-diretorio = os.listdir('.')
+class Application:
+    def selec_button():
+        diretorio = filedialog.askdirectory()
+        ### Comando que vai para a pasta destino onde estão os arquivos ###
+        os.chdir(diretorio)
+        ### Comando que lista os arquivos que estão no diretório ###
+        diretorio = os.listdir('.')
+        num = 1
 
-num = 1
-### Para cada arquivo no diretório ###
-for arquivo in diretorio:
-    ### Extraio um vetor contendo os dados e a taxa de amostragem ###
-    dados, samplerate = sf.read('%s'%arquivo)
-    ### Crio uma lista vazia ###
-    data_list = []
+        ### Para cada arquivo no diretório ###
+        for arquivo in diretorio:
+            ### Extraio um vetor contendo os dados e a taxa de amostragem ###
+            dados, samplerate = sf.read('%s'%arquivo)
+            ### Crio uma lista vazia ###
+            data_list = []
 
-    ### Testo se é uma matriz ou um vetor, se sim: ###
-    if dados[0].size == 2:
-        ### Para cada amostra na matriz orignal ###
-        for amostra in dados:
-            ### Adiciono um valor correspondente a média dos dois valores na lista vazia ###
-            data_list.append((amostra[0] + amostra[1])/2)
+            ### Testo se é uma matriz ou um vetor, se sim: ###
+            if dados[0].size == 2:
+                ### Para cada amostra na matriz orignal ###
+                for amostra in dados:
+                    ### Adiciono um valor correspondente a média dos dois valores na lista vazia ###
+                    data_list.append((amostra[0] + amostra[1])/2)
 
-        ### Transformo a lista em um vetor (objeto) através da biblioteca "numpy" ###
-        data_mono = np.array(data_list)
-        ### Edito o arquivo transformando-o em "mono" ###
-        sf.write('%s'%arquivo, data_mono,samplerate)
+                ### Transformo a lista em um vetor (objeto) através da biblioteca "numpy" ###
+                data_mono = np.array(data_list)
+                ### Edito o arquivo transformando-o em "mono" ###
+                sf.write('%s'%arquivo, data_mono, samplerate)
 
-    ### Se não: ###
-    else:
-        ### Digo que o arquivo não é estéreo ###
-        print('O arquivo "%s" não é estéreo!!'%arquivo)
+            ### Se não: ###
+            else:
+                ### Digo que o arquivo não é estéreo ###
+                print('O arquivo "%s" não é estéreo!!'%arquivo)
 
-    ### Caso o nome do arquivo seja diferente do padrão: ###
-    if arquivo != '%d.wav'%num:        
-        ### Renomeio o arquivo original no padrão "n.wav", onde "n" é um inteiro ###
-        os.rename('%s'%arquivo,'%d.wav'%num)
-        ### Digo que o arquivo (e o novo nome dele) não é estéreo ###
-        print('O arquivo "%s" foi renomeado para %d.wav"'%(arquivo, num))
+            ### Caso o nome do arquivo seja diferente do padrão: ###
+            if arquivo != '%d.wav'%num:
+                ### Renomeio o arquivo original no padrão "n.wav", onde "n" é um inteiro ###
+                os.rename('%s'%arquivo, '%d.wav'%num)
+                ### Digo que o arquivo (e o novo nome dele) não é estéreo ###
+                print('O arquivo "%s" foi renomeado para %d.wav"'%(arquivo, num))
+            num += 1
+        arquivo = str(input("Digite o nome do arquivo que deseja plotar no gráfico: "))
+        ### Para cada arquivo no diretório (após a modificação) ###
+        for arq in os.listdir('.'):
+            if arquivo == arq:
+                ### Seleciono e abro o arquivo .wav digitado em questão no modo somente leitura ###
+                musica = wave.open('%s'%arq, 'r')
+                ### Leio todos os frames (amostras) do áudio, transformando-os em strings ###
+                form_onda = musica.readframes(-1)
+                ### Inicializo um vetor através de uma string ###
+                form_onda = np.fromstring(form_onda, 'Int16')
+                ### Obtenho a frequência de amostragem da música ###
+                fa = musica.getframerate()
+                ### Faço a divisão da quantidade total de amostras pela fa
+                # para obter o tempo (em segundos) ###
+                tempo = np.linspace(0, len(form_onda)/fa, num=len(form_onda))
+                ### Afim de obter a amplitude com referência em tensão (-1 a +1V),
+                # divido os níveis de quantização por (2^16)/2 ###
+                amplit = form_onda/32768
+                ### Faço a plotagem gráfico da amplitude em função do tempo ###
+                plt.xlabel("Tempo")
+                plt.ylabel("Amplitude")
+                plt.title('Formas de onda do arquivo %s'%arq)
+                plt.plot(tempo, amplit)
+                plt.show()
+                
+    ### Rotina para reiniciar o shell do Python ###
+    def restart():
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+        selec_button()
 
-    num +=1
+    ### Rotina do Tkinter para os botões e os rótulos ###
+    root = Tk()
+    label1 = Label(master = root, text = "Selecione a pasta que deseja manipular os arquivos de áudio")
+    label1.grid(padx = 0, pady = 0)
+    button1 = Button(text = "Selecionar pasta", command = selec_button)
+    button1.grid(padx = 0, pady = 10)
+    quit = Button(text = "Fechar", command = root.destroy)
+    quit.grid(padx = 0, pady = 10)
+    restart = Button(master = root, text = "Deu ruim? Clique aqui", command = restart)
+    restart.grid(padx = 150, pady = 10)
 
-arquivo = str(input("Digite o nome do arquivo que deseja plotar no gráfico: "))
-### Para cada arquivo no diretório (após a modificação) ###
-for arq in os.listdir('.'):
-    if arquivo == arq:
-        ### Seleciono e abro o arquivo .wav digitado em questão no modo somente leitura ###
-        musica = wave.open('%s'%arq,'r')
-        ### Leio todos os frames (amostras) do áudio, transformando-os em strings ###
-        form_onda = musica.readframes(-1)
-        ### Inicializo um vetor através de uma string ###
-        form_onda = np.fromstring(form_onda, 'Int16')
-        ### Obtenho a frequência de amostragem da música ###
-        fa = musica.getframerate()
-        ### Faço a divisão da quantidade total de amostras pela fa
-        # para obter o tempo (em segundos) ###
-        tempo = np.linspace(0, len(form_onda)/fa, num=len(form_onda))
-        ### Afim de obter a amplitude com referência em tensão (-1 a +1V),
-        # divido os níveis de quantização por (2^16)/2 ###
-        amplit = form_onda/32768
-        ### Faço a plotagem gráfico da amplitude em função do tempo ###
-        plt.xlabel("Tempo")
-        plt.ylabel("Amplitude")
-        plt.title('Formas de onda do arquivo %s'%arq)
-        plt.plot(tempo, amplit)
-        plt.show()
+    root.mainloop()
